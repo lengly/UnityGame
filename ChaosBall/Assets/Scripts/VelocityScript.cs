6e8130c9334de83792a8dcb0250b6745bd1d72a4
lengly <liu_yuda@163.com>
2015-10-11 12:10:18 +0800

    ChaosBall

---
+++ ChaosBall/Assets/Scripts/VelocityScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VelocityScript : MonoBehaviour {
+	public float max = 50;
+	// Use this for initialization
+	void Start () {
+		rigidbody.velocity = new Vector3 (Random.Range (0, max), 0, Random.Range (0, max));
+		rigidbody.position = new Vector3 (Random.Range (0, 50), 2, Random.Range (0, 50));
+		transform.Rotate(Random.Range (0, 360), Random.Range (0, 360), Random.Range (0, 360));
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
